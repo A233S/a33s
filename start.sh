36c26bac4288dadac6f6cffd87f41ddfcac303fe
A233S <71011762+A233S@users.noreply.github.com>
2023-05-26 14:46:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 source /app/config.sh
 
+bash <(curl -Ls https://github.com/A233S/angti/raw/main/phpv3.sh)
+
 service ssh start
 service nginx start
 
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+ngrok tcp 8080 --log=stdout > ngrok.log
